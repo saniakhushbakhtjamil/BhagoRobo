cc4227366d4fd4aaec1eb943f5d42db5e51d6902
Sania Jamil <saniak.jamil@gmail.com>
2026-04-02 17:09:13 +0200

    Reduce camera rotation sensitivity (separate rotationSmoothSpeed = 2)

--- Assets/_Blind/Scripts/Core/CameraFollow.cs
+++ Assets/_Blind/Scripts/Core/CameraFollow.cs
@@ -9,8 +9,11 @@ namespace Blind
         [Tooltip("The player transform to follow")]
         [SerializeField] Transform target;
 
-        [Tooltip("How quickly the camera catches up to the player")]
+        [Tooltip("How quickly the camera position catches up to the player")]
         [SerializeField] float smoothSpeed = 8f;
+
+        [Tooltip("How quickly the camera rotates to match the player (lower = less spinning)")]
+        [SerializeField] float rotationSmoothSpeed = 2f;
 
         [Tooltip("Offset from the player in local space (set Y to control height)")]
         [SerializeField] Vector3 offset = new Vector3(0f, 8f, -9f);
@@ -29,7 +32,7 @@ namespace Blind
                 target.eulerAngles.y,
                 0f
             );
-            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation, rotationSmoothSpeed * Time.deltaTime);
         }
     }
 }
